c7ce59575ed6d46f638f9ba59765c75e411b3aea
Chinmay Manjunath <cmanjun@ncsu.edu>
2017-04-07 00:44:58 -0400

    Create CacheStore.py

---
+++ CacheStore.py
@@ -0,0 +1,10 @@
+from collections import defaultdict
+import threading
+
+
+class FileMeta(object):
+    
+    access_count_map = defaultdict(int)
+    write_count_map = defaultdict(int)
+    path_to_uuid_map = defaultdict(int)
+    lock_map = defaultdict(threading.RLock)
