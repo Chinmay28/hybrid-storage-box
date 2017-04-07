f4f8a80734c2dacd138cb9315f630122782dadb3
Chinmay Manjunath <cmanjun@ncsu.edu>
2017-04-07 00:44:21 -0400

    Moved writeToDB out to DBUtil

--- FuseWrapper.py
+++ FuseWrapper.py
@@ -15,44 +15,16 @@ import pprint
 import threading
 import thread
 import time
+from CacheStore import FileMeta
 
-#DB update thread
-def WriteToDB(classobj, sleeptime):
-    while True:
-        #Wake up every sleeptime seconds
-        time.sleep(sleeptime)
-        print("Writing file counts to DB (one by one)")
-        #Write to DB only if any dictionary is not empty
-        if len(classobj.file_access_count) > 0 or len(classobj.file_write_count) > 0:
-            #loop on all file locks
-            for path in classobj.path_to_uuid:
-                realpath = classobj.getrealpath(path)
-                uuid = classobj.path_to_uuid[realpath]
-                lock = classobj.file_lock[uuid]
-                lock.acquire()
-                #only write if atleast one count is non zero. (This condition should not occur)
-                if classobj.file_access_count[uuid] > 0 or classobj.file_write_count[uuid] > 0:
-                    #write to DB
-                    update_query = "update file_meta set access_count=\'" + str(classobj.file_access_count[uuid])+"\', write_count=\'" \
-                    + str(classobj.file_write_count[uuid])+"\' where file_id=\'" + str(uuid)+ "\';"
-                    #print ("Updated", update_query)
-                    classobj.db_conn.insert(update_query)
 
-                lock.release()
-            #Clear both count dictionaries. Note that we dont need to clear lock dictionary.
-            classobj.file_access_count.clear()
-            classobj.file_write_count.clear()
-
-class Passthrough(Operations):
+class FuseSystem(Operations):
+    
     def __init__(self, root):
         self.root = root
         self.db_conn = DBUtil()
-        self.file_access_count = defaultdict(lambda:0)
-        self.file_write_count = defaultdict(lambda:0)
-        self.file_lock = defaultdict(threading.RLock)
-        self.path_to_uuid = defaultdict(lambda:0)
         #Start DB update thread. Pass self pointer and sleep time.
-        thread.start_new_thread(WriteToDB, (self, 10))
+        thread.start_new_thread(DBUtil.writeToDB, (self, 10))
 
     # Helpers
     # =======
@@ -142,7 +114,7 @@ class Passthrough(Operations):
         realpath = self.getrealpath(self._full_path(old))
 
         #Aquire lock so that policy thread wont interfere
-        lock = self.file_lock[self.path_to_uuid[realpath]]
+        lock = FileMeta.lock_map[FileMeta.path_to_uuid_map[realpath]]
         lock.acquire()
         retval = os.rename(self._full_path(old), self._full_path(new))
         #unlick now
@@ -165,7 +137,7 @@ class Passthrough(Operations):
         realpath = self.getrealpath(full_path)
         print("LOG: open ", realpath)
         #Aquire lock so that policy thread wont interfere. Unlock in release method
-        lock = self.file_lock[self.path_to_uuid[realpath]]
+        lock = FileMeta.lock_map[FileMeta.path_to_uuid_map[realpath]]
         lock.acquire()
         return os.open(full_path, flags)
 
@@ -175,7 +147,7 @@ class Passthrough(Operations):
         file_id = str(uuid.uuid1())
 
         #Aquire lock so that policy thread wont interfere. Unlock in release method
-        lock = self.file_lock[file_id]
+        lock = FileMeta.lock_map[file_id]
         lock.acquire()
 
         print("LOG create(): ", full_path)
@@ -186,16 +158,16 @@ class Passthrough(Operations):
         volume_info = "hdd_hot"
         file_tag = "tada!"
         query = "insert into file_meta values( \'" + file_id+"\', \'" \
-        + full_path+"\', \'" + create_time+ "\', \'" + last_update_time+"\', \'" + last_move_time\
+        + self.getrealpath(full_path) +"\', \'" + create_time+ "\', \'" + last_update_time+"\', \'" + last_move_time\
         +"\', \'" + access_count+"\', \'" + write_count+"\', \'" + volume_info+"\', \'" + file_tag+"\');"
         self.db_conn.insert(query)
 
         #Add path and uuid to dictionary
-        self.path_to_uuid[full_path] = file_id
+        FileMeta.path_to_uuid_map[self.getrealpath(full_path)] = file_id
 
         #update usage count
-        self.file_access_count[file_id] += 1
-        self.file_write_count[file_id] += 1
+        FileMeta.access_count_map[file_id] += 1
+        FileMeta.write_count_map[file_id] += 1
 
 
         return os.open(full_path, os.O_WRONLY | os.O_CREAT, mode)
@@ -205,7 +177,7 @@ class Passthrough(Operations):
         print("LOG: read ", realpath)
         os.lseek(fh, offset, os.SEEK_SET)
         #update read count
-        self.file_access_count[self.path_to_uuid[realpath]] += 1
+        FileMeta.access_count_map[FileMeta.path_to_uuid_map[realpath]] += 1
         return os.read(fh, length)
 
     def write(self, path, buf, offset, fh):
@@ -213,8 +185,8 @@ class Passthrough(Operations):
         print("LOG: write ", realpath)
         os.lseek(fh, offset, os.SEEK_SET)
         #update read and write count
-        self.file_access_count[self.path_to_uuid[realpath]] += 1
-        self.file_write_count[self.path_to_uuid[realpath]] += 1
+        FileMeta.access_count_map[FileMeta.path_to_uuid_map[realpath]] += 1
+        FileMeta.write_count_map[FileMeta.path_to_uuid_map[realpath]] += 1
         return os.write(fh, buf)
 
     def truncate(self, path, length, fh=None):
@@ -231,7 +203,7 @@ class Passthrough(Operations):
         realpath = self.getrealpath(self._full_path(path))
         print("LOG: release ", realpath)
         #Unlock the lock taken during open or create.
-        lock = self.file_lock[self.path_to_uuid[realpath]]
+        lock = FileMeta.lock_map[FileMeta.path_to_uuid_map[realpath]]
         lock.release()
         return os.close(fh)
 
@@ -244,7 +216,7 @@ class Passthrough(Operations):
         return os.path.realpath(path)
 
 def main(mountpoint, root):
-    FUSE(Passthrough(root), mountpoint, nothreads=True, foreground=True)
+    FUSE(FuseSystem(root), mountpoint, nothreads=True, foreground=True)
 
 
 if __name__ == '__main__':
