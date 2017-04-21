0bff952d322d80daff3ac5f47af6ab8154064c3a
ssjoshi2 <ssjoshi2@ncsu.edu>
2017-04-21 14:42:39 -0400

    Add disk name to path map and default disk DS

--- CacheStore.py
+++ CacheStore.py
@@ -1,11 +1,18 @@
 from collections import defaultdict
 import threading
 
-
 class FileMeta(object):
-    
+
     access_count_map = defaultdict(int)
     write_count_map = defaultdict(int)
     path_to_uuid_map = defaultdict(int)
     lock_map = defaultdict(threading.RLock)
-    
+
+    disk_to_path_map = {
+        "io1" : "./io1",
+        "gp2" : "./gp2",
+        "st1" : "./st1",
+        "sc1" : "./sc1"
+    }
+
+    DEFAULT_DISK = "st1"
